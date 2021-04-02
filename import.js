e7a5e28386181af1ca423b41cc4eaa85f70cb8e7
Nguyễn Trọng Khang <79756284+trongkhang020801@users.noreply.github.com>
2021-04-03 00:30:07 +0700

    Add files via upload

---
+++ import.js
@@ -0,0 +1,18 @@
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
+const inputs = document.querySelectorAll(".input");
+
+
+function addcl(){
+	let parent = this.parentNode.parentNode;
+	parent.classList.add("focus");
+}
